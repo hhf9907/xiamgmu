193a7912c9186650576c15c6716ffe4f8337fa15
hhf7 <6503729+hhf7@user.noreply.gitee.com>
2020-03-17 01:41:01 +0800

    666

--- src/network/request.js
+++ src/network/request.js
@@ -8,9 +8,9 @@ export function request(config) {
   })
   //2.axios的拦截器
   instance.interceptors.request.use(config =>{
-    //1.比如说config中的一些信息不符合服务器要求
-    //2.比如每次发送网络请求时，都希望在界面中显示一个请求的图标
-    //3.比如某些网络请求，（比如登录（token）），必须携带一些特殊的信息
+    //1.config中的一些信息不符合服务器要求
+    //2.每次发送网络请求时，都希望在界面中显示一个请求的图标
+    //3.某些网络请求，（比如登录（token）），必须携带一些特殊的信息
     return config
   },err =>{
   })
